e52754b597e9983a85efeb9599ed47e5c436afbd
abhishekahuja87 <60549112+abhishekahuja87@users.noreply.github.com>
2020-04-04 16:18:58 +0530

    Add files via upload
    responsive commit-2

--- src/App.js
+++ src/App.js
@@ -1,156 +1,55 @@
 import React, { Component } from "react";
-import MainPage from "./ReactComponents/MainPage";
-import MainNav from "./ReactComponents/MainNav";
-import Contact from "./ReactComponents/Contact";
+import HeaderAndSider from "./ReactComponents/HeaderAndSider";
 import {
-  BrowserRouter,
-  Switch,
-  Route,
-  Redirect,
-  HashRouter
-} from "react-router-dom";
-import Items from "./ReactComponents/Items";
-import { message } from "antd";
+  Layout,
+  Affix,
+  Menu,
+  Breadcrumb,
+  Avatar,
+  Badge,
+  Row,
+  Col,
+  Drawer,
+  Button,
+  Card,
+  InputNumber,
+  Statistic,
+  Form,
+  Input,
+  Checkbox,
+  DatePicker,
+  message
+} from "antd";
+import {
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  CrownOutlined,
+  EnvironmentOutlined,
+  PieChartOutlined,
+  DesktopOutlined,
+  TeamOutlined,
+  FileOutlined,
+  ShoppingOutlined
+} from "@ant-design/icons";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      wishListedItems:
-        JSON.parse(localStorage.getItem("allWishListedItems")) || [],
-      cartItems: JSON.parse(localStorage.getItem("allCartItems1")) || []
-    };
+    this.state = {};
   }
 
   render() {
-    // console.log(window.location.search);
-    console.log(this.state.wishListedItems);
-    // return <h2>sdsdsd</h2>;
     return (
-      <div>
-        <MainNav
-          wishListedItems={this.state.wishListedItems}
-          cartItems={this.state.cartItems}
-          removeFromCart={this.removeFromCart}
-          removeFromWishlist={this.removeFromWishlist}
-          removeAllFromCart={this.removeAllFromCart}
-        />
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-          {/* <HashRouter basename="/"> */}
-          <Switch>
-            <Route path="/" exact component={MainPage} />
-            <Route path="/contact" component={Contact} />
-            <Route
-              path="/items/:category/:subCategory"
-              // component={Items}
-              render={props => (
-                <Items
-                  {...props}
-                  isAuthed="propIpassed"
-                  wishListedItems={this.state.wishListedItems}
-                  cartItems={this.state.cartItems}
-                  addToWishList={this.addToWishList}
-                  removeFromWishlist={this.removeFromWishlist}
-                  addToCart={this.addToCart}
-                />
-              )}
-            />
-
-            {/* <Route path="/products/:id" component={ProductDetails} />
-            <Route
-              path="/products"
-              render={props => <Products sortBy="newest" {...props} />}
-            />
-            <Route path="/posts/:year?/:month?" component={Posts} />
-            <Route path="/admin" component={Dashboard} />
-            <Redirect from="/messages" to="/posts" />
-            <Route path="/not-found" component={NotFound} />
-            <Route path="/" exact component={Home} />
-            <Redirect to="/not-found" /> */}
-          </Switch>
-        </BrowserRouter>
-        {/* </HashRouter> */}
-      </div>
+      <HeaderAndSider
+        wishListedItems={this.state.wishListedItems}
+        cartItems={this.state.cartItems}
+        removeFromCart={this.removeFromCart}
+        removeFromWishlist={this.removeFromWishlist}
+        removeAllFromCart={this.removeAllFromCart}
+      />
     );
   }
-
-  addToWishList = item => {
-    message.info("Added to Wishlist");
-    console.log(item);
-    let wishListedItemsL = this.state.wishListedItems;
-    wishListedItemsL.push(item);
-    this.setState({ wishListedItems: wishListedItemsL });
-
-    //store to local storage
-    localStorage.setItem(
-      "allWishListedItems",
-      JSON.stringify(this.state.wishListedItems)
-    );
-  };
-
-  // removeFromWishList = item => {
-  //   console.log(item);
-  //   let wishListedItemsL = this.state.wishListedItems;
-  //   wishListedItemsL.pop(item);
-  //   this.setState({ wishListedItems: wishListedItemsL });
-
-  //   //store to local storage
-  //   localStorage.setItem(
-  //     "allWishListedItems",
-  //     JSON.stringify(this.state.wishListedItems)
-  //   );
-  // };
-
-  addToCart = item => {
-    message.info("Added to Cart");
-    console.log(item);
-    let cartItemsL = this.state.cartItems;
-    cartItemsL.push(item);
-    this.setState({ cartItems: cartItemsL });
-
-    //store to local storage
-    localStorage.setItem("allCartItems1", JSON.stringify(this.state.cartItems));
-  };
-
-  removeAllFromCart = () => {
-    // message.info("Removed All");
-    // console.log(item);
-    // let cartItemsL = this.state.cartItems;
-    // cartItemsL.forEach(item=>{
-    //   cartItemsL.p
-    // })
-    // cartItemsL.pop(item);
-    let a = [];
-    this.setState({ cartItems: a });
-
-    //store to local storage
-    localStorage.setItem("allCartItems1", JSON.stringify(a));
-  };
-
-  removeFromCart = item => {
-    message.info("Removed from cart");
-    console.log(item);
-    let cartItemsL = this.state.cartItems;
-    cartItemsL.pop(item);
-    this.setState({ cartItems: cartItemsL });
-
-    //store to local storage
-    localStorage.setItem("allCartItems1", JSON.stringify(this.state.cartItems));
-  };
-
-  removeFromWishlist = item => {
-    message.info("Removed from wishlist");
-    console.log(item);
-    let wishListedItemsL = this.state.wishListedItems;
-    wishListedItemsL.pop(item);
-    this.setState({ wishListedItems: wishListedItemsL });
-
-    //store to local storage
-    localStorage.setItem(
-      "allWishListedItems",
-      JSON.stringify(this.state.wishListedItems)
-    );
-  };
 }
 
 export default App;
